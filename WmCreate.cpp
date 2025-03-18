e4b52bd1cf808d7b3e23a58a7376d62f104ac468
김호 <hindkind9674@gmail.com>
2025-03-18 15:14:38 +0900

    Visual Studio 솔루션 및 윈도우 생성 코드 추가
    ApiExam_1.sln에 솔루션 파일 형식과 프로젝트 구성 정보가 추가되었습니다.
    ApiExam_1.vcxproj에 "Client.cpp"와 "WmCreate.cpp" 파일이 추가되었으며, 이 파일들은 "Debug|x64" 구성에서 빌드에서 제외되었습니다.
    ApiExam_1.vcxproj.filters에 해당 파일에 대한 필터가 추가되었습니다.
    Client.cpp와 WmCreate.cpp 파일에 Windows API를 사용하여 윈도우를 생성하고 메시지 루프를 처리하는 코드가 추가되었습니다.
    메모리 할당 및 해제를 포함한 메시지 처리 로직도 구현되었습니다.

---
+++ WmCreate.cpp
@@ -0,0 +1,54 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("WmCreate");
+TCHAR* mem;
+int APIENTRY WinMain(HINSTANCE hinstance, HINSTANCE hPrevlnstance, LPSTR lpszCmdParam, int nCmdShow) {
+
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hinstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hinstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, (WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL),
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hinstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+
+	switch (iMessage) {
+	case WM_CREATE:
+		mem = (TCHAR*)malloc(1048567);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		free(mem);
+		return 0;
+	case WM_LBUTTONDOWN:
+		MessageBeep(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
