f4223d0287a86dcda57bdb953cba4cac4ebad125
김호 <hindkind9674@gmail.com>
2025-03-18 17:01:13 +0900

    새로운 타이머 및 키 입력 처리 기능 추가
    `ApiExam_1.vcxproj`에 `Client.cpp`, `KeyDown2.cpp`, `TimerText.cpp`, `TimerText2.cpp` 파일을 추가하고, 이들 파일의 빌드 설정을 조정했습니다.
    `KeyDown2.cpp`는 키 입력을 처리하는 기능을 구현하였고, `TimerText.cpp`와 `TimerText2.cpp`는 타이머를 설정하여 현재 시간을 화면에 출력하는 기능을 추가했습니다.
    `TimerText2.cpp`는 출력 위치를 변경하는 기능을 포함하고 있습니다.
    또한, 새로 추가된 소스 파일들이 `ApiExam_1.vcxproj.filters`에 포함되었습니다.

---
+++ TimerText2.cpp
@@ -0,0 +1,74 @@
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("MY_TIMER");
+
+int APIENTRY WinMain(HINSTANCE hinstance, HINSTANCE hPrevlnstance, LPSTR lpszCmdParam, int nCmdShow) {
+
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hinstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hinstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, (WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL),
+		300, 100,
+		CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hinstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+
+	HDC hdc;
+	PAINTSTRUCT ps;
+	SYSTEMTIME st;
+	static TCHAR sTime[126];
+	const static TCHAR* str[3] = { TEXT("첫 번째 문자열"), TEXT("두 번째 문자열"), TEXT("세 번째 문자열") };
+	static int y = 100;
+	switch (iMessage) {
+	case WM_DESTROY:
+		KillTimer(hWnd, 1);
+		PostQuitMessage(0);
+		return 0;
+	case WM_LBUTTONDOWN:
+		MessageBeep(0);
+		return 0;
+	case WM_CREATE:
+		SetTimer(hWnd, 1, 2000, NULL);
+		SendMessage(hWnd, WM_TIMER, 1, 0);
+		return 0;
+	case WM_TIMER:
+		GetLocalTime(&st);
+		wsprintf(sTime, TEXT("%s"), str[rand() % 3]);
+		InvalidateRect(hWnd, NULL, TRUE);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		for (int i = 100; i > 10; i--)
+			TextOut(hdc, 10, i, sTime, lstrlen(sTime));
+		EndPaint(hWnd, &ps);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
